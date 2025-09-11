5469ed6b9e86ac28bfb5c0f1bcf8414d2e304419
theolacerda-create <theolacerda@gmail.com>
2025-09-11 16:09:55 -0300

    commit-6.0

--- js/armas.js
+++ js/armas.js
@@ -1,10 +1,10 @@
 const track = document.querySelector('.carousel-track');
 const items = document.querySelectorAll('.arma');
 
-let currentIndex = 1; 
+let currentIndex = 0; 
 
 function updateCarousel() {
-  const itemWidth = items[0].offsetWidth + 100;
+  const itemWidth = items[0].offsetWidth + 60; 
   const offset = (track.parentElement.offsetWidth / 2) - (items[currentIndex].offsetWidth / 2);
 
   track.style.transform = `translateX(${-currentIndex * itemWidth + offset}px)`;
@@ -14,14 +14,32 @@ function updateCarousel() {
   });
 }
 
-updateCarousel();
-
-
-items.forEach((item, i) => {
-  item.addEventListener('click', () => {
-    currentIndex = i;
+function goToNext() {
+  if (currentIndex < items.length - 1) {
+    currentIndex++;
     updateCarousel();
-  });
-});
+  }
+}
+
+function goToPrevious() {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+}
+
+
+document.querySelector('.carousel').addEventListener('wheel', (event) => {
+  if (event.deltaY > 0) {
+    goToNext();
+  } else {
+    goToPrevious();
+  }
+  event.preventDefault(); 
+}, { passive: false });
+
 
 window.addEventListener('resize', updateCarousel);
+
+
+updateCarousel();
